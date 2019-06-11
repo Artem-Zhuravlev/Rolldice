bcbe3e1e52d97ae044d2f71a170549e97227effb
Artem <mstrJesm@gmail.com>
2019-06-11 14:52:28 +0300

    Add Rolldice component

--- src/components/Die/Die.js
+++ src/components/Die/Die.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import './Die.css';
 
 class Die extends Component {
     render() {
+        let cls = `Die fas fa-dice-${this.props.face}`;
         return (
-            <div>
-                
-            </div>
+            
+            <i className={cls}/>
         );
     }
 }
